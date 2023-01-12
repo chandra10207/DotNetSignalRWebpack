3ce3321df35a6801325db4b9ea9ca2c5bdc74e9b
Chandra Shekhar <chandra.pandey@winning.com.au>
2023-01-13 10:19:44 +1100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// app.UseDefaultFiles();
+// app.UseStaticFiles();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
